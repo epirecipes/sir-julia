d77685ec44f81839ea28d163b8b160e27dc75903
slwu89 <slwu89@users.noreply.github.com>
2023-02-10 15:30:22 -0800

    doesn't seem possible to do what I want to do

--- tutorials/markov_measure/tmp.jl
+++ tutorials/markov_measure/tmp.jl
@@ -1,4 +1,5 @@
 using MeasureTheory
+import Distributions
 using Plots
 
 # mc = Chain(x -> Normal(μ=x), Normal(μ=0.0))
@@ -18,3 +19,23 @@ mc = Chain(x -> counting(x), Dirac([0,0]))
 r = rand(mc)
 samp = Iterators.take(r, 1000)
 plot(transpose(hcat(collect(samp)...)))
+
+
+rate2prob(x) = Distributions.cdf(Distributions.Exponential(), x)
+
+function sir_trans(x, pars)
+    β, γ, Δt = pars
+    S, I, R = x
+    si_prob = rate2prob(I*β*Δt)
+    ir_prob = rate2prob(γ*Δt)
+    si_rv = Binomial(S, si_prob)
+    ir_rv = Binomial(I, ir_prob)
+    [x[1]-si_rv, x[2]+si_rv-ir_rv, x[3]+ir_rv]
+end
+
+mc = Chain(x -> sir_trans, Dirac([990, 10, 0]))
+r = rand(mc)
+samp = Iterators.take(r, 1000)
+
+breakpoint(sir_trans)
+
