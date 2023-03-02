50e70801cac5a236253aebab0e8cbeed30ff1d20
slwu89 <slwu89@users.noreply.github.com>
2023-03-01 19:06:31 -0800

    use AffineTransform and pushfwd

--- script/markov_measure/markov_measure.jl
+++ script/markov_measure/markov_measure.jl
@@ -11,25 +11,27 @@ using BenchmarkTools
 end;
 
 
-function sir_markov!(x, state, p)
+function sir_markov!(x, u0, p)
     (β,c,γ,δt) = p
 
+    S, I, R = u0
     @inbounds begin
-        state[1] -= x[1]
-        state[2] += x[1]
-        state[2] -= x[2]
-        state[3] += x[2]
+        S -= x[1]
+        I += x[1]
+        I -= x[2]
+        R += x[2]
     end
-
-    S, I, R = state
     N = S+I+R
 
     si_prob = rate_to_proportion(β*c*I/N, δt)
     ir_prob = rate_to_proportion(γ, δt)
     si_rv = MeasureTheory.Binomial(S, si_prob)
     ir_rv = MeasureTheory.Binomial(I, ir_prob)
+
+    N_inf = AffineTransform((μ=x[1],))
+    N_rec = AffineTransform((μ=x[2],))
     
-    productmeasure([si_rv, ir_rv])
+    productmeasure([pushfwd(N_inf, si_rv), pushfwd(N_rec, ir_rv)])
 end
 
 
@@ -48,23 +50,18 @@ u0 = [990,10,0]; # S,I,R
 p = [0.05,10.0,0.25,δt]; # β,c,γ,δt
 
 
-Random.seed!(1234);
+Random.seed!(123);
 
 
-state = deepcopy(u0);
-
-
-mc = Chain(x -> sir_markov!(x, state, p), productmeasure([Dirac(0), Dirac(0)]))
+mc = Chain(x -> sir_markov!(x, u0, p), productmeasure([Dirac(0), Dirac(0)]))
 r = rand(mc)
 samp = Iterators.take(r, nsteps)
 sir_trace = collect(samp)
 sir_trace = transpose(hcat(sir_trace...))
 
 
-sir_out = zeros(Int, nsteps+1, 3)
-sir_out[1,:] = u0
+sir_out = transpose(hcat(fill(u0, nsteps+1)...))
 for i in 2:nsteps+1
-    sir_out[i,:] = sir_out[i-1,:] 
     sir_out[i,1] -= sir_trace[i-1,1]
     sir_out[i,2] += sir_trace[i-1,1]
     sir_out[i,2] -= sir_trace[i-1,2]
