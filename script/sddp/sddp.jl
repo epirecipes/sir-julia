8f22bc68f864fc0932850ab61408bb1d5c4432fa
slwu89 <slwu89@users.noreply.github.com>
2023-05-09 18:45:13 -0700

    work on SDDP notebook

--- script/sddp/sddp.jl
+++ script/sddp/sddp.jl
@@ -1,21 +1,26 @@
-using SDDP, JuMP, Ipopt, Plots
 
-tmax = 100.0
-δt = 1.0
-nsteps = Int(tmax / δt)
+using SDDP, JuMP, HiGHS, Plots;
 
-u0 = [0.99, 0.01] # S,I
+
 β = 0.5 # infectivity rate
 γ = 0.25 # recovery rate
 υ_max = 0.5 # maximum intervention
-υ_total = 10.0 # maximum cost
-opt=Ipopt.Optimizer
+υ_total = 10.0; # maximum cost
+
+
+tmax = 100.0
+δt = 1.0
+nsteps = Int(tmax / δt);
+
+
+u0 = [0.99, 0.01]; # S,I
+
 
 model = SDDP.LinearPolicyGraph(
     stages = nsteps,
     sense = :Min,
     lower_bound = 0,
-    optimizer = opt,
+    optimizer = HiGHS.Optimizer,
 ) do sp, t
 
     @variable(sp, 0 ≤ S, SDDP.State, initial_value = u0[1])
@@ -23,12 +28,10 @@ model = SDDP.LinearPolicyGraph(
     @variable(sp, 0 ≤ C, SDDP.State, initial_value = 0)
 
     @variable(sp, 0 ≤ υ_cumulative, SDDP.State, initial_value = 0)
-    # @variable(sp, 0 ≤ υ ≤ υ_max, start = 0)
     @variable(sp, 0 ≤ υ ≤ υ_max)
 
     # constraints on control    
     @constraint(sp, υ_cumulative.out == υ_cumulative.in + (δt * υ))
-    # @constraint(sp, υ_cumulative.out ≤ υ_total)
     @constraint(sp, υ_cumulative.in + (δt * υ) ≤ υ_total)
 
     # expressions to simplify the state updates
@@ -40,22 +43,17 @@ model = SDDP.LinearPolicyGraph(
     @NLconstraint(sp, I.out == I.in + infection - recovery)
     @NLconstraint(sp, C.out == C.in + infection)
 
-    if t == nsteps
-        @stageobjective(sp, C.out)
-    else
-        @stageobjective(sp, 0)
-    end    
+    # linear weighting of objectives
+    @stageobjective(sp, υ_cumulative.out + 40*C.out)
 
-end
+end;
 
-SDDP.train(model; iteration_limit = 200)
 
-# simulate from the optimal policy
-sims = SDDP.simulate(model, 1, [:S,:I, :C, :υ, :υ_cumulative])
+SDDP.train(model; iteration_limit = 100);
 
-# traj = [[sims[1][i][:S].in,sims[1][i][:I].in,sims[1][i][:υ]] for i in 1:nsteps]
-# traj = transpose(hcat(traj...))
-# plot(traj)
+
+sims = SDDP.simulate(model, 1, [:S,:I, :C, :υ, :υ_cumulative]);
+
 
 Plots.plot(
     SDDP.publication_plot(sims, title = "S") do data
@@ -76,11 +74,3 @@ Plots.plot(
     xlabel = "Time"
 )
 
-# final cumulative infections
-sims[1][nsteps][:C]
-
-# # debugging
-# sp_moi = JuMP.read_from_file("./subproblem_67.mof.json")
-
-# sp_moi = JuMP.read_from_file("./subproblem_22.mof.json")
-# JuMP.latex_formulation(sp_moi)
